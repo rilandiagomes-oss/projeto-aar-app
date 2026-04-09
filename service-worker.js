090678419d51c41602d81c901f14107d4ec32f86
rilandiagomes-oss <rilandiagomes@gmail.com>
2026-04-09 19:43:38 -0300

    Create service-worker.js
    codigo novo

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "aar-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./animais-transmissores-raiva.html",
+  "./assets/style.css",
+  "./assets/script.js",
+  "./assets/ConditionalContent.js",
+  "./assets/PopupTrigger.js",
+  "./assets/PopupButton.js",
+  "./assets/PopupModal.js",
+  "./assets/RandomMovImage.js",
+  "./assets/AutoOpenPopupModal.js",
+  "./assets/img/morcego.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
